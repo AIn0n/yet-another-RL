85e0480d4b3fe15bc1b0608edf61582a1c98ad1f
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2019-12-17 08:47:15 +0100

    WIP

--- python-tic-tac-toe-Policy-RL/init.py
+++ python-tic-tac-toe-Policy-RL/init.py
@@ -3,7 +3,7 @@ from ticTacToe import *
 import time
 
 ALPHA = 0.1
-FI = 0.00000001
+FI = 0.01
 
 rl = reinf_learning(ALPHA, FI)
 game = TicTacToe()
@@ -15,17 +15,19 @@ def policy_eval_rec(rl, game, char):
         next_state_set = game.next_state_x()
     else:
         next_state_set = []
+        curr_state = game.tab
         aux = game.next_state_o()
         for state in aux:
             game.tab = state
             next_state_set += game.next_state_x()
+        game.tab = curr_state
 
     for state in next_state_set:
 
         win = game.check_state(state)
-        if win == 1:    rl.change_prize( state, 10)
-        elif win == -1: rl.change_prize( state, -10)
-        elif win == 0:  rl.change_prize( state, -1)
+        if win == 1:    rl.change_prize( state, 5.0)
+        elif win == -1: rl.change_prize( state, -5.0)
+        elif win == 0:  rl.change_prize( state, -1.0)
 
         if win == -2:
 
@@ -44,11 +46,56 @@ def policy_eval_rec(rl, game, char):
 
     rl.policy_evaluation(next_state_set)
 
-time_counter = time.time()
-policy_eval_rec(rl, game, "X")
-time_counter = time.time() - time_counter
-game.reset()
-print("===\nGOTOWE\n===\nczas = ", time_counter)
+def policy_imprv_rec(rl, game, char):
+    if char == 'X':
+        next_state_set = game.next_state_x()
+    else:
+        next_state_set = []
+        aux = game.next_state_o()
+        for state in aux:
+            game.tab = state
+            next_state_set += game.next_state_x()
+        game.reset()
+
+    for state in next_state_set:
+
+        win = game.check_state(state)
+
+        if win == -2:
+
+            curr_state = game.curr_state()
+
+            rl.prev_state = state
+            game.tab = state
+
+            if char == 'X':
+                policy_eval_rec(rl, game, 'O')
+            else:
+                policy_eval_rec(rl, game, 'X')
+
+            rl.prev_state = curr_state
+            game.tab = curr_state
+
+    rl.policy_improvement(next_state_set)
+
+while rl.is_policy_stable == False:
+
+    old_delta = 0.0
+    while True:
+        time_counter = time.time()      #do liczenia czasu
+        rl.delta = 0.0                  #zawsze zaczyna od zerowej delty
+        policy_eval_rec(rl, game, "X")  #tutaj rekurencyjnie policy evaluation
+        time_counter = time.time() - time_counter   #tez do liczenia czasu
+        print("===\nGOTOWE\n===\nczas = ", time_counter)
+        #jezeli delta nieznacznie sie zmienila
+        #to mozemy sobie przejsc do policy improvment
+        if(rl.delta == old_delta): break
+        old_delta = rl.delta
+        game.reset()
+
+    rl.is_policy_stable = True
+    policy_imprv_rec(rl, game, "X")  #tutaj rekurencyjnie policy improvment
+    game.reset()
 
 #=========================KOD NIZEJ SLUZY DO GRY===============================
 
@@ -63,6 +110,7 @@ while tmp != 0:
             while game.postaw_znak_o(int(input("podaj x => ")), int(input("podaj y => "))) == 1:
                 print("podano zle liczby!")
         else:               
+            rl.show_prizes(game.next_state_x())
             game.tab = rl.check_possible_states(game.next_state_x())
             print("ruch komputera")
 
--- python-tic-tac-toe-Policy-RL/reinforcementLearning.py
+++ python-tic-tac-toe-Policy-RL/reinforcementLearning.py
@@ -6,6 +6,7 @@ class reinf_learning:
         self.prev_state = 0     #poprzedni stan
         self.is_policy_stable = False      #true jezeli policy jest stabilne
         self.fi =  usr_fi             #to powinna byc mala, dodatnia liczba
+        self.delta = 0.0
 
     def change_prize(self, curr_state, newPrize):
         self.prize_dict[curr_state] = [newPrize, False]
@@ -41,33 +42,29 @@ class reinf_learning:
                 self.prize_dict[state] = [0.0, False]
 
         if self.prev_state not in self.prize_dict:
-            self.prize_dict[self.prev_state] = [0.0, False]
+            self.prize_dict[self.prev_state] = [0.0, random.choice(states)]
         
         for state in states:
-
-            delta = 0.0
 
             old_prize = self.prize_dict[self.prev_state][0]
             self.prize_dict[self.prev_state][0] += (self.alpha * (self.prize_dict[state][0] - self.prize_dict[self.prev_state][0]))
 
-            delta = max(delta, abs( old_prize - self.prize_dict[self.prev_state][0]))
+            self.delta = max(self.delta, abs( old_prize - self.prize_dict[self.prev_state][0]))
 
-            if delta < self.fi: self.is_policy_stable = True
-            else:               self.is_policy_stable = False
+            if self.delta < self.fi:
+                return 1
 
-        if self.prize_dict[self.prev_state][1] == False:
-            self.prize_dict[self.prev_state][1] = random.choice(states)
 
-        return states
+        return 0
 
 
     def policy_improvement(self, states):
         for state in states:
 
             old_action = self.prize_dict[self.prev_state][1]
-            self.prize_dict[self.prev_state][1] = max(self.prize_dict, key=self.prize_dict.get)            
 
-            if old_action != self.prize_dict[self.prev_state][1]: self.is_policy_stable = False
-
-        if self.is_policy_stable == False:
-            policy_evaluation(states)
+            biggest_prize = max( [self.prize_dict[x] for x in states] )
+            biggest_prize_states = [x for x in states if self.prize_dict[x] == biggest_prize]
+            if old_action not in biggest_prize_states:
+                self.prize_dict[self.prev_state][1] = random.choice(biggest_prize_states)
+                self.is_policy_stable = False
